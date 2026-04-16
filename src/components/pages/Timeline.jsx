1ef75ade24b8fca278fb29614b1fea4826d15507
Saidul Islam <youremail@example.com>
2026-04-17 02:18:58 +0600

    Filter Timeline box added

--- src/components/pages/Timeline.jsx
+++ src/components/pages/Timeline.jsx
@@ -1,16 +1,54 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { FriendContext } from "../context/FriendContext";
 
 const Timeline = () => {
   const { timeline } = useContext(FriendContext);
 
+  const [filter, setFilter] = useState("All");
+
+  // filtered data
+  const filteredTimeline =
+    filter === "All"
+      ? timeline
+      : timeline.filter((item) => item.type === filter);
+
   return (
-    <div className="bg-gray-200 py-6 sm:py-8  px-3 sm:px-5">
-      
-      {timeline.map((item, index) => (
+    <div className="bg-gray-200 py-6 sm:py-8 px-3 sm:px-5">
+
+      {/* Filter Box */}
+      <div className="max-w-6xl mx-auto mb-5">
+  <div className="relative w-full sm:w-80">
+    <select
+      value={filter}
+      onChange={(e) => setFilter(e.target.value)}
+      className="w-full appearance-none px-4 py-3 rounded-xl border border-gray-300 bg-white text-gray-700 text-sm shadow-sm focus:outline-none focus:ring-2"
+    >
+      <option value="All">Filter Timeline</option>
+      <option value="Call">Call</option>
+      <option value="Text">Text</option>
+      <option value="Video">Video</option>
+    </select>
+
+    {/* Dropdown Icon */}
+    <div className="absolute inset-y-0 right-3 flex items-center pointer-events-none">
+      <svg
+        className="w-4 h-4 text-gray-500"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        viewBox="0 0 24 24"
+      >
+        <path d="M19 9l-7 7-7-7" />
+      </svg>
+    </div>
+  </div>
+</div>
+
+      {/* Timeline List */}
+      {filteredTimeline.map((item, index) => (
         <div
           key={index}
-          className="bg-white max-w-6xl mx-auto p-4 sm:p-5 md: mb-3 rounded-xl shadow-sm"
+          className="bg-white max-w-6xl mx-auto p-4 sm:p-5 mb-3 rounded-xl shadow-sm"
         >
           <div className="flex flex-col sm:flex-row sm:items-center gap-3 sm:gap-5">
             
